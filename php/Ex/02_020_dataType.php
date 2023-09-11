9708ff4fe04157f95c186ef542ce67f289a3f097
muhwa91 <muhwa_91@naver.com>
2023-09-11 17:36:43 +0900

    0911_PHP

---
+++ php/Ex/02_020_dataType.php
@@ -0,0 +1,27 @@
+<?php
+// int : 숫자
+$num = 1;
+// string : 문자열
+$str = "sssss";
+// array : 배열
+$array = [1, 2, 3];
+// double : 실수
+$double = 2.343;
+// boolean : 논리(참/거짓)
+$bool = true; //$bool = false;
+// NULL
+$obj = null;
+
+// gettype : 변수의 타입 리턴
+// echo gettype($str);
+
+$num1 = 1;
+$str1 = "1";
+
+// echo $num1 + (int)$str1;
+// 변수 앞에 형변환 가능
+// 형변환 : 변수 앞에 (데이터타입)$num
+echo gettype($num1);
+
+
+?>
---
+++ php/Ex/02_025_switch.php
@@ -0,0 +1,22 @@
+<?php
+// switch문
+// 조건에 따라서 서로 다른 처리를 할 수 있음
+// 값이 고정되어 있는 경우 if문 대신 사용할 수 있음
+// case-break 적어주기
+$food = "떡볶이";
+
+switch( $food ) {
+    case "김밥":
+        echo "한식";
+        break;
+    case "마파두부":
+        echo "중식";
+        break;
+    default:
+        echo "기타";
+        break;
+}
+
+
+
+?>
