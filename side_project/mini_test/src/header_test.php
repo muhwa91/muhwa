ef58e01c597dd6bbf49d5b12c387a03b54dcb8e9
muhwa91 <muhwa91@gmail.com>
2023-10-05 01:12:01 +0900

    1005_PHP

--- side_project/mini_test/src/header_test.php
+++ side_project/mini_test/src/header_test.php
@@ -5,7 +5,146 @@
 			<span></span>
 		</label>
 		<input type="checkbox" id="checkbox">
-		<h1>mini_test</h1>
+		
+	<div class="rain_eff">
+		<div class="rain">
+			<div class="drop"></div>
+			<div class="waves">
+				<div></div>
+				<div></div>
+			</div>
+			<div class="splash"></div>
+			<div class="particles">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+
+		<div class="rain">
+			<div class="drop"></div>
+			<div class="waves">
+				<div></div>
+				<div></div>
+			</div>
+			<div class="splash"></div>
+			<div class="particles">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+
+		<div class="rain">
+			<div class="drop"></div>
+			<div class="waves">
+				<div></div>
+				<div></div>
+			</div>
+			<div class="splash"></div>
+			<div class="particles">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+
+		<div class="rain">
+			<div class="drop"></div>
+			<div class="waves">
+				<div></div>
+				<div></div>
+			</div>
+			<div class="splash"></div>
+			<div class="particles">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+
+		<div class="rain">
+			<div class="drop"></div>
+			<div class="waves">
+				<div></div>
+				<div></div>
+			</div>
+			<div class="splash"></div>
+			<div class="particles">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+
+		<div class="rain">
+			<div class="drop"></div>
+			<div class="waves">
+				<div></div>
+				<div></div>
+			</div>
+			<div class="splash"></div>
+			<div class="particles">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+
+		<div class="rain">
+			<div class="drop"></div>
+			<div class="waves">
+				<div></div>
+				<div></div>
+			</div>
+			<div class="splash"></div>
+			<div class="particles">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+
+		<div class="rain">
+			<div class="drop"></div>
+			<div class="waves">
+				<div></div>
+				<div></div>
+			</div>
+			<div class="splash"></div>
+			<div class="particles">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+
+		<div class="rain">
+			<div class="drop"></div>
+			<div class="waves">
+				<div></div>
+				<div></div>
+			</div>
+			<div class="splash"></div>
+			<div class="particles">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	</div>
+
+	<h1>mini_test</h1>
+
 </header>
 <nav>
 	<div class="back color-5">    	    
@@ -21,144 +160,7 @@
   	</div>		
 </nav>
 
-<div class="rain eff">
 
-<div class="rain">
-	<div class="drop"></div>
-	<div class="waves">
-		<div></div>
-		<div></div>
-	</div>
-	<div class="splash"></div>
-	<div class="particles">
-		<div></div>
-		<div></div>
-		<div></div>
-		<div></div>
-	</div>
-</div>
-
-<div class="rain">
-	<div class="drop"></div>
-	<div class="waves">
-		<div></div>
-		<div></div>
-	</div>
-	<div class="splash"></div>
-	<div class="particles">
-		<div></div>
-		<div></div>
-		<div></div>
-		<div></div>
-	</div>
-</div>
-
-<div class="rain">
-	<div class="drop"></div>
-	<div class="waves">
-		<div></div>
-		<div></div>
-	</div>
-	<div class="splash"></div>
-	<div class="particles">
-		<div></div>
-		<div></div>
-		<div></div>
-		<div></div>
-	</div>
-</div>
-
-<div class="rain">
-	<div class="drop"></div>
-	<div class="waves">
-		<div></div>
-		<div></div>
-	</div>
-	<div class="splash"></div>
-	<div class="particles">
-		<div></div>
-		<div></div>
-		<div></div>
-		<div></div>
-	</div>
-</div>
-
-<div class="rain">
-	<div class="drop"></div>
-	<div class="waves">
-		<div></div>
-		<div></div>
-	</div>
-	<div class="splash"></div>
-	<div class="particles">
-		<div></div>
-		<div></div>
-		<div></div>
-		<div></div>
-	</div>
-</div>
-
-<div class="rain">
-	<div class="drop"></div>
-	<div class="waves">
-		<div></div>
-		<div></div>
-	</div>
-	<div class="splash"></div>
-	<div class="particles">
-		<div></div>
-		<div></div>
-		<div></div>
-		<div></div>
-	</div>
-</div>
-
-<div class="rain">
-	<div class="drop"></div>
-	<div class="waves">
-		<div></div>
-		<div></div>
-	</div>
-	<div class="splash"></div>
-	<div class="particles">
-		<div></div>
-		<div></div>
-		<div></div>
-		<div></div>
-	</div>
-</div>
-
-<div class="rain">
-	<div class="drop"></div>
-	<div class="waves">
-		<div></div>
-		<div></div>
-	</div>
-	<div class="splash"></div>
-	<div class="particles">
-		<div></div>
-		<div></div>
-		<div></div>
-		<div></div>
-	</div>
-</div>
-
-<div class="rain">
-	<div class="drop"></div>
-	<div class="waves">
-		<div></div>
-		<div></div>
-	</div>
-	<div class="splash"></div>
-	<div class="particles">
-		<div></div>
-		<div></div>
-		<div></div>
-		<div></div>
-	</div>
-</div>
-
-</div>
 
 
 
